f45c915d590bf8ddf2df66490390f18ef4ddf2fd
cuubeo <cuubeo36@gmal.com>
2024-04-02 09:42:33 +0700

    Register
    Code

--- src/test/java/com/parabank/pageObject/RegisterPage.java
+++ src/test/java/com/parabank/pageObject/RegisterPage.java
@@ -11,14 +11,29 @@ public class RegisterPage {
     public RegisterPage(WebDriver driver){
         this.driver = driver;
     }
-
     By txtFirstName = By.id("customer.firstName");
-
     By txtLastName = By.id("customer.lastName");
-
-    public void register(String firstName, String lastName){
+    By txtAddress = By.id("customer.address.street");
+    By txtCity = By.id("customer.address.city");
+    By txtState = By.id("customer.address.state");
+    By txtZipCode = By.id("customer.address.zipCode");
+    By txtPhoneNo = By.id("customer.phoneNumber");
+    By txtSsn = By.id("customer.ssn");
+    By txtUsername = By.id("customer.username");
+    By txtPassword = By.id("customer.password");
+    By txtRePass = By.id("repeatedPassword");
+    public void register(String firstName, String lastName, String address, String city, String state, String zipCode, String phoneNo, String Ssn, String username, String password, String rePass){
         log.info("Registering user");
         driver.findElement(txtFirstName).sendKeys(firstName);
         driver.findElement(txtLastName).sendKeys(lastName);
+        driver.findElement(txtAddress).sendKeys(address);
+        driver.findElement(txtCity).sendKeys(city);
+        driver.findElement(txtState).sendKeys(state);
+        driver.findElement(txtZipCode).sendKeys(zipCode);
+        driver.findElement(txtPhoneNo).sendKeys(phoneNo);
+        driver.findElement(txtSsn).sendKeys(Ssn);
+        driver.findElement(txtUsername).sendKeys(username);
+        driver.findElement(txtPassword).sendKeys(password);
+        driver.findElement(txtRePass).sendKeys(rePass);
     }
 }
--- src/test/java/com/parabank/test/RegisterTest.java
+++ src/test/java/com/parabank/test/RegisterTest.java
@@ -1,5 +1,4 @@
 package com.parabank.test;
-
 import com.parabank.pageObject.HomePage;
 import com.parabank.pageObject.RegisterPage;
 import com.parabank.pojo.User;
@@ -8,27 +7,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
-public class RegisterTest {
+import java.time.Duration;
 
+public class RegisterTest {
     @Test
     public void testRegister() {
         User user = new User();
         user.setFirstName("John");
         user.setLastName("Doe");
+        user.setAddress("Hill");
+        user.setCity("LA");
+        user.setState("US");
+        user.setZipCode("12345");
+        user.setPhoneNumber("090011111111");
+        user.setSsn("123");
+        user.setUsername("JohnD");
+        user.setPassword("P12345678");
+        user.setConfirmPassword("P12345678");
 
         WebDriverManager.edgedriver().setup();
         WebDriver driver = new EdgeDriver();
-
         HomePage homePage = new HomePage(driver);
-
         String baseUrl = homePage.baseUrl;
-
         driver.get(baseUrl);
         homePage.clickRegisterLink();
-
         RegisterPage registerPage = new RegisterPage(driver);
-        registerPage.register(user.getFirstName(), user.getLastName());
-
+        registerPage.register(user.getFirstName(),
+                user.getLastName(),
+                user.getAddress(),
+                user.getCity(),
+                user.getState(),
+                user.getZipCode(),
+                user.getPhoneNumber(),
+                user.getSsn(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getConfirmPassword());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.quit();
     }
 }
