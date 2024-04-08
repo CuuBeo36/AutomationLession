fe710d9f27fd698951c312aa2bb73c80a1add0af
cuubeo <cuubeo36@gmal.com>
2024-04-08 09:20:54 +0700

    add cases + fix comments

--- src/test/java/com/parabanknew/test/RegisterTest.java
+++ src/test/java/com/parabanknew/test/RegisterTest.java
@@ -10,7 +10,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
@@ -26,30 +29,25 @@ public class RegisterTest {
     @BeforeTest
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        // Disable save password popup
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("autofill.profile_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.get(baseUrl);
     }
 
-    @Test(priority = 1)
+    @Test (priority = 5)
     public void testRegisterSuccess() throws InterruptedException {
         User user = new User();
         user.generateUser();
 
         HomePage homePage = new HomePage(driver);
         homePage.clickRegister();
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-web-security");
-        options.addArguments("--no-proxy-server");
-
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("profile.password_manager_enabled", false);
-
-        options.setExperimentalOption("prefs", prefs);
 
         RegisterPage registerPage = new RegisterPage(driver);
         registerPage.Register(user);
@@ -62,55 +60,97 @@ public class RegisterTest {
         String actualElementText = t.getText();
         System.out.println(actualElementText);
         Assert.assertEquals(actualElementText, expectedElementText, "Expected and Actual are not same");
-
-
     }
 
-    @Test(priority = 2)
-    public void testRegisterEmptyUsername() throws InterruptedException {
+    @Test (priority = 2)
+    public void testRegisterUsernameEmpty() throws InterruptedException {
         User user = new User();
         user.generateUser();
+        user.setUsername("");
 
         HomePage homePage = new HomePage(driver);
         homePage.clickRegister();
 
         RegisterPage registerPage = new RegisterPage(driver);
         registerPage.Register(user);
-        user.setPassword("Passw@rd");
 
-        String expectedElementText = "Username is empty.";
+
+        //        TODO: expectedElementText is wrong, pls check -> DONE
+        String expectedElementText = "Username is required.";
         Thread.sleep(5000);
 
-        WebElement t = driver.findElement(By.xpath("//*[@id=\"rightPanel\"]/p"));
+        //        TODO: xpath should be in pageObject -> DONE
+        WebElement t = driver.findElement(By.id("customer.username.errors"));
 
         String actualElementText = t.getText();
         System.out.println(actualElementText);
-        Assert.assertEquals(actualElementText, expectedElementText, "Expected and Actual are not same");
+
+        //        TODO: error message should be more clear and more specific -> DONE
+        Assert.assertEquals(actualElementText, expectedElementText, "Wrong message in case username is empty");
     }
 
-
-    @Test(priority = 3)
-    public void testRegisterPasswordUnmatch() throws InterruptedException {
+    @Test (priority = 3)
+    public void testRegisterPasswordEmpty() throws InterruptedException {
         User user = new User();
         user.generateUser();
-
-        HomePage homePage = new HomePage(driver);
-        homePage.clickRegister();
-
-        RegisterPage registerPage = new RegisterPage(driver);
-        registerPage.Register(user);
         user.setPassword("");
 
-        String expectedElementText = "Confirm Password is not match";
+        HomePage homePage = new HomePage(driver);
+        homePage.clickRegister();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.Register(user);
+
+
+//        TODO: expectedElementText is wrong, pls check -> DONE
+        String expectedElementText = "Password is required.";
         Thread.sleep(5000);
 
-        WebElement t = driver.findElement(By.xpath("//*[@id=\"rightPanel\"]/p"));
+//        TODO: xpath should be in pageObject -> DONE
+        WebElement t = driver.findElement(By.id("customer.password.errors"));
 
         String actualElementText = t.getText();
         System.out.println(actualElementText);
-        Assert.assertEquals(actualElementText, expectedElementText, "Expected and Actual are not same");
+
+//        TODO: error message should be more clear and more specific -> DONE
+        Assert.assertEquals(actualElementText, expectedElementText, "Wrong message in case password is empty");
     }
-        @AfterTest
+
+    @Test (priority = 4)
+    public void testRegisterUsernameExist() throws InterruptedException {
+        User user = new User();
+        user.generateUser();
+        user.setFirstName("Seymour");
+        user.setUsername("Seymour");
+
+        HomePage homePage = new HomePage(driver);
+        homePage.clickRegister();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.Register(user);
+
+
+//        TODO: expectedElementText is wrong, pls check -> DONE
+        String expectedElementText = "This username already exists.";
+        Thread.sleep(5000);
+
+//        TODO: xpath should be in pageObject -> DONE
+        WebElement t = driver.findElement(By.id("customer.username.errors"));
+
+        String actualElementText = t.getText();
+        System.out.println(actualElementText);
+
+//        TODO: error message should be more clear and more specific -> DONE
+        Assert.assertEquals(actualElementText, expectedElementText, "Wrong message in case username is existed");
+    }
+
+
+//    @AfterMethod
+//    public void goBackToHomepage ( ) {
+//        driver.findElement(By.linkText("Home")).click() ;
+//    }
+
+    @AfterTest
         public void tearDown() {
             driver.quit();
         }
